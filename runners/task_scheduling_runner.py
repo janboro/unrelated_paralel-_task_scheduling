b99c1483651ca7f1fcbfb18a7dbb7a867a679323
Jan Borowiec <janekborowiec@gmail.com>
2022-05-07 18:27:06 +0200

    Updated runner

--- runners/task_scheduling_runner.py
+++ runners/task_scheduling_runner.py
@@ -1,3 +1,4 @@
+import numpy as np
 from task_generator.task_scheduling_generator import UnrelatedParallelMachineSchedulingGenerator
 from algorithms.particle_swarm_optimization.operators import Operators
 from algorithms.shortest_release_date.shortest_release_date import ShortestReleaseDates
@@ -10,8 +11,18 @@ def main():
     SRD = ShortestReleaseDates(problem=problem)
     SRD.assign_jobs()
 
-    A = [3, 4, 1, 2, "*", 5, 7, 6]
-    print(operators.multiply(A=A))
+    print(problem.machines)
+    vector_solution = operators.vectorize_solution(machines=problem.machines)
+    R = list(np.random.binomial(n=1, p=0.5, size=len(vector_solution)))
+    operators.multiply(A=R, B=vector_solution, problem=problem)
+
+    print(problem.machines)
+
+    print(operators.vectorize_solution(machines=problem.machines))
+    print()
+
+    # A = [3, 4, 1, 2, "*", 5, 7, 6]
+    # print(operators.multiply(A=A))
 
 
 if __name__ == "__main__":
