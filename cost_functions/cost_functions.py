358a8bfcd4374bc1a406d512e22589f29b3685bc
Jan Borowiec <janekborowiec@gmail.com>
2022-05-12 19:14:05 +0200

    Plotting gantt plot

--- cost_functions/cost_functions.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def sphere(values: np.ndarray):
-    return np.sum(np.power(values, 2))
-
-
-def parabole(values: np.ndarray):
-    return 5 * values[0] ** 2 + 3 * values[1] + values[2]
--- utils/plotter.py
+++ utils/plotter.py
@@ -2,69 +2,41 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
-class Plotter:
-    def __init__(self, no_of_jobs):
-        self.no_of_jobs = no_of_jobs
-        self.colors = self.generate_colors(no_of_jobs=no_of_jobs)
+def generate_color():
+    return tuple(np.random.randint(256, size=3) / 256)
 
-    def generate_colors(self, no_of_jobs):
-        colors = []
-        for _ in range(no_of_jobs):
-            colors.append(tuple(np.random.randint(256, size=3) / 256))
-        return colors
 
-    def gantt_plot(self, machines, title):
-        plt.figure()
-        plt.title(title)
-        for machine in machines.itertuples():
-            for job in machine.assigned_jobs:
-                plt.barh(
-                    y=f"M{machine.Index}",
-                    left=job["start_time"],
-                    width=job["end_time"] - job["start_time"],
-                    color=self.colors[job["job_index"]],
-                    label=f"J{job['job_index']}",
-                )
+def gantt_plot(scheduling_solution, title, plot_label=True):
+    plt.figure()
+    plt.title(title)
+    for machine in scheduling_solution.machines.itertuples():
+        processing_time = 0
+        for job in machine.assigned_jobs:
+            print(f"Machine: {machine.Index}")
+            print(f"Job: {job}")
+            start_time = max(processing_time, scheduling_solution.jobs.loc[job, "release_date"])
+            end_time = start_time + scheduling_solution.processing_times.loc[machine.Index, job]
+            processing_time = end_time
+            color = generate_color()
 
-        plt.gca().invert_yaxis()
+            plt.barh(
+                y=f"M{machine.Index}",
+                left=start_time,
+                width=end_time - start_time,
+                color=color,
+                label=f"J{job}",
+            )
 
-        # Plotting handles
-        handles, labels = plt.gca().get_legend_handles_labels()
-        handle_list, label_list = [], []
-        for handle, label in zip(handles, labels):
-            if label not in label_list:
-                handle_list.append(handle)
-                label_list.append(label)
+    plt.gca().invert_yaxis()
+
+    # Plotting handles
+    handles, labels = plt.gca().get_legend_handles_labels()
+    handle_list, label_list = [], []
+    for handle, label in zip(handles, labels):
+        if label not in label_list:
+            handle_list.append(handle)
+            label_list.append(label)
+    if plot_label:
         plt.legend(handle_list, label_list)
-        plt.xlim(left=0)
-        plt.show()
-
-    def job_shop_gantt_plot(self, machines, title):
-        plt.figure()
-        plt.title(title)
-        for machine in machines.itertuples():
-            for operation in machine.assigned_jobs:
-                print(operation)
-                print(operation["job"])
-                print(self.colors)
-                print(self.colors[operation["job"]])
-                plt.barh(
-                    y=f"M{machine.Index}",
-                    left=operation["start_time"],
-                    width=operation["end_time"] - operation["start_time"],
-                    color=self.colors[operation["job"]],
-                    label=f"J{operation['job']}",
-                )
-
-        plt.gca().invert_yaxis()
-
-        # Plotting handles
-        handles, labels = plt.gca().get_legend_handles_labels()
-        handle_list, label_list = [], []
-        for handle, label in zip(handles, labels):
-            if label not in label_list:
-                handle_list.append(handle)
-                label_list.append(label)
-        plt.legend(handle_list, label_list)
-        plt.xlim(left=0)
-        plt.show()
+    plt.xlim(left=0)
+    plt.show()
